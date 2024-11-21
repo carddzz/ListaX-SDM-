eadee8eb1fc8f4b5ad7fb3eb17ff2b57fe911436
Cláudio Albertini <claudioalbertini2502@gmail.com>
2024-11-21 18:43:37 -0300

    Lista 10

---
+++ ConsumerAdviceApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsumerAdviceApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using HttpClient client = new HttpClient();
+                string jsonResponse = await client.GetStringAsync("https://api.adviceslip.com/advice");
+
+               
+                var adviceData = JsonDocument.Parse(jsonResponse);
+                string advice = adviceData.RootElement.GetProperty("slip").GetProperty("advice").GetString() ?? "Conselho indisponível";
+
+                
+                Console.WriteLine("Conselho de Hoje:");
+                Console.WriteLine(advice);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro: " + ex.Message);
+            }
+        }
+    }
+}
